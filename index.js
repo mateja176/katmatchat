7184712bdaac819c93f59cb1f4cca81b05006914
mytee306 <mateja176@gmail.com>
2018-08-23 21:30:56 +0200

    index.js: sends index from root instead of docs

--- index.js
+++ index.js
@@ -2,38 +2,38 @@ var app = require( 'express' )();
 var http = require( 'http' ).Server( app );
 var io = require( 'socket.io' )( http );
 
-const { Client } = require('pg');
-const pg = require('pg');
+const { Client } = require( 'pg' );
+const pg = require( 'pg' );
 pg.defaults.ssl = true;
-const client = new Client({
+const client = new Client( {
   connectionString: 'postgres://nxfyibeerzlslk:fc2e63648042c6f5f43e955ce52e994148978568b61c77e70ad7ee8c5f6962dc@ec2-54-228-251-254.eu-west-1.compute.amazonaws.com:5432/d88vsglfasevbg',
-  ssl: true,
-});
+  ssl: true
+} );
 
 
 app.get( '/', function ( req, res ) {
-  res.sendFile( `${ __dirname }/docs/index.html` );
+  res.sendFile( `${ __dirname }/index.html` );
 } );
 
 io.on( 'connection', function ( socket ) {
 
   client.connect();
   const query = {
-    text: 'SELECT message FROM Messages',
-    
+    text: 'SELECT message FROM Messages'
+
   }
-  
-  client.query(query, (err, res) => {
-    if (err) {
-      console.log(err.stack)
+
+  client.query( query, ( err, res ) => {
+    if ( err ) {
+      console.log( err.stack )
     } else {
-      socket.on( 'result', function ( res ) {       
-        io.emit('result', res.rows[0]);
+      socket.on( 'result', function ( res ) {
+        io.emit( 'result', res.rows[ 0 ] );
       } );
     }
 
     client.end();
-  })
+  } )
 
   console.log( 'a user connected' );
 
@@ -43,7 +43,7 @@ io.on( 'connection', function ( socket ) {
 
   socket.on( 'chat message', function ( msg ) {
     console.log( 'message: ' + msg );
-    io.emit('chat message', msg);
+    io.emit( 'chat message', msg );
   } );
 
 } );
@@ -51,7 +51,5 @@ io.on( 'connection', function ( socket ) {
 const port = process.env.PORT || 3000;
 
 http.listen( port, function () {
-  console.log( `listening on port: ${port}` );
-});
-
-
+  console.log( `listening on: http://localhost:${ port }` );
+} );
